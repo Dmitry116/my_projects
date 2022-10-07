efc9e3800f8f1576696cb87b9eccb3fbe13b1480
Дмитрий Румянцвев <dimon_16rus@bk.ru>
2022-10-07 17:51:25 +0000

    Update Module20/04_players/main.py

--- Module20/04_players/main.py
+++ Module20/04_players/main.py
@@ -1,18 +1,12 @@
+players = {
+    ("Ivan", "Volkin"): (10, 5, 13),
+    ("Bob", "Robbin"): (7, 5, 14),
+    ("Rob", "Bobbin"): (12, 8, 2)
+}
+
+tuple_players = [names + value for names, value in players.items()]
+print(tuple_players )
+
+
+
 # TODO здесь писать код
-
-def slicer(nums, num):
-    if num in nums:
-        num_index_start = nums.index(num)
-        num_revers = nums[::-1]
-        num_index_end = num_revers.index(num)
-        index_end = (len(nums) - num_index_end - 1)
-        if num <= nums.count(num):
-            return (nums[num_index_start:index_end])
-        else:
-            return (nums[num_index_start::])
-    else:
-        return ()
-
-
-number = int(input('Введите номер: '))
-print(slicer((1, 2, 3, 4, 5, 6, 7, 8, 2, 2, 9, 10), number))
