a206878213b6f9b3bc7929e61896defd8d54426f
Professional <dimon_16rus@bk.ru>
2022-12-19 19:49:20 +0300

    home_work_m24. done 6

--- Module24/06_magic/main.py
+++ Module24/06_magic/main.py
@@ -1,51 +1,110 @@
 # TODO здесь писать код
+import random
 
-elements = {
-    1: 'Вода',
-    2: 'Воздух',
-    3: 'Огонь',
-    4: 'Земля'
-}
-
-"""
-Вода + Воздух = Шторм
-Вода + Огонь = Пар
-Вода + Земля = Грязь
-Воздух + Огонь = Молния
-Воздух + Земля = Пыль
-Огонь + Земля = Лава
-"""
 
 class Water:
-    print('Вода')
+    def __str__(self):
+        water = 'Вода'
+        return water
+
+    def __add__(self, other):
+        if isinstance(other, Earth):
+            return Dirt()
+        elif isinstance(other, Air):
+            return Storm()
+        elif isinstance(other, Fire):
+            return Steam()
+
 
 class Air:
-    print('Воздух')
+    def __str__(self):
+        air = 'Воздух'
+        return air
+
+    def __add__(self, other):
+        if isinstance(other, Earth):
+            return Steam()
+        elif isinstance(other, Water):
+            return Storm()
+        elif isinstance(other, Fire):
+            return Lightning()
+
 
 class Fire:
-    print('Огонь')
+    def __str__(self):
+        fire = 'Огонь'
+        return fire
 
-class Ground:
-    print('Земля')
-
-water = Water()
-air = Air()
-fire = Fire()
-ground = Ground()
-
-class Example_1:
     def __add__(self, other):
-        return Example_2()
+        if isinstance(other, Earth):
+            return Lava()
+        elif isinstance(other, Water):
+            return Steam()
+        elif isinstance(other, Air):
+            return Lightning()
 
 
-class Example_2:
-    answer = 'сложили два класса и вывели'
+class Earth:
+    def __str__(self):
+        earth = 'Земля'
+        return earth
+
+    def __add__(self, other):
+        if isinstance(other, Fire):
+            return Lava()
+        elif isinstance(other, Water):
+            return Dirt()
+        elif isinstance(other, Air):
+            return Dust()
 
 
-a = Example_1()
-d = Example_1()
-b = Example_2()
+class Storm:
+    def __str__(self):
+        storm = 'Шторм'
 
-s = Example_2()
-c = a + s
-print(c.answer)
+        return storm
+
+
+class Steam:
+    def __str__(self):
+        steam = 'Пар'
+        return steam
+
+
+class Dirt:
+
+    def __str__(self):
+        dirt = 'Грязь'
+        return dirt
+
+
+class Dust:
+    def __str__(self):
+        dust = 'Пыль'
+        return dust
+
+
+class Lava:
+    def __str__(self):
+        lava = 'Лава'
+        return lava
+
+
+class Lightning:
+    def __str__(self):
+        lightning = 'Молния'
+        return lightning
+
+
+elements = {
+    1: Water(),
+    2: Air(),
+    3: Fire(),
+    4: Earth()
+}
+
+elem_1 = elements[random.randint(1, 4)]
+elem_2 = elements[random.randint(1, 4)]
+elem_3 = elem_1 + elem_2
+print(f'{elem_1} + {elem_2} = {elem_3}')
+print()
