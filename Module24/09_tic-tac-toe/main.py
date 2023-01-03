416fcb4ae34a875d6e467ebb9fe41d2431429c56
Professional <dimon_16rus@bk.ru>
2023-01-04 01:21:44 +0300

    home_work_m24. done 9

--- Module24/09_tic-tac-toe/main.py
+++ Module24/09_tic-tac-toe/main.py
@@ -1,41 +1,112 @@
 # TODO здесь писать код
 class Cells:
-    board = {
+    """Пустые клетки"""
+    cells = {
         7: ' ', 8: ' ', 9: ' ',
         4: ' ', 5: ' ', 6: ' ',
         1: ' ', 2: ' ', 3: ' '
     }
 
-    def print(self=None):
+    """Выигрышные комбинации"""
+    movies = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        [1, 5, 9],
+        [3, 5, 7]
+    ]
+
+
+class Board:
+    def board(self=None):
         print("\n")
         print("\t     |     |")
-        print("\t  {}  |  {}  |  {}".format(Cells.board[7], Cells.board[8], Cells.board[9]))
+        print("\t  {}  |  {}  |  {}".format(Cells.cells[7], Cells.cells[8], Cells.cells[9]))
         print('\t_____|_____|_____')
 
         print("\t     |     |")
-        print("\t  {}  |  {}  |  {}".format(Cells.board[4], Cells.board[5], Cells.board[6]))
+        print("\t  {}  |  {}  |  {}".format(Cells.cells[4], Cells.cells[5], Cells.cells[6]))
         print('\t_____|_____|_____')
 
         print("\t     |     |")
-        print("\t  {}  |  {}  |  {}".format(Cells.board[1], Cells.board[2], Cells.board[3]))
+        print("\t  {}  |  {}  |  {}".format(Cells.cells[1], Cells.cells[2], Cells.cells[3]))
         print("\t     |     |")
         print("\n")
 
 
-def game(i):
-    Cells.print()
-    try:
-        a = int(input(f'Ход {i} куда ставить: '))
+class Player:
+    name = ''
+    dice = ''
 
-        if Cells.board[a] != ' ':
-            print('Клетка занята. Выберите другую клетку.')
-            return game(i)
+    def print(self):
+        print('Имя: {}\n'
+              'Фишка: {}\n'.format(self.name, self.dice))
 
-        Cells.board[a] = i
-    except KeyError:
-        print('Не правильный номер клетки. Попробуйте еще раз.')
-        return game(i)
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Game:
+    def game(self, name, dice):
+        Board.board()
+        try:
+            position = int(input(f'Ход {name} куда ставить фишку "{dice}": '))
+
+            if Cells.cells[position] != ' ':
+                print('Клетка занята. Выберите другую клетку.')
+                return Game.game(self, name, dice)
+
+            Cells.cells[position] = dice
+
+        except KeyError:
+            print('Не правильный номер клетки. Попробуйте еще раз.')
+            return Game.game(self, name, dice)
+
+    """Проверка на выигрыш"""
+
+    def winner(self=None):
+        win = ''
+        for i in Cells.movies:
+            if Cells.cells[i[0]] == 'X' and Cells.cells[i[1]] == 'X' and Cells.cells[i[2]] == 'X':
+                win = 'X'
+            if Cells.cells[i[0]] == '0' and Cells.cells[i[1]] == '0' and Cells.cells[i[2]] == '0':
+                win = '0'
+
+        return win
+
+    """Проверка на ничью"""
+
+    def free_cells(self=None):
+        count = 0
+        for key in Cells.cells:
+            if Cells.cells[key] == ' ':
+                count += 1
+        if count == 0:
+            Board.board()
+            print('Игра закончилась ничьей.')
+            raise SystemExit
+
+
+player_1 = Player()
+player_1.name = 'Игрок-1'
+player_1.dice = 'X'
+player_2 = Player()
+player_2.name = 'Игрок-2'
+player_2.dice = '0'
+
+player_1.print()
+player_2.print()
+players = player_1, player_2
 
 while True:
-    for i in ('X', '0'):
-        game(i)
+    for player in players:
+        Game.game('', player.name, player.dice)
+        Game.winner()
+        if Game.winner():
+            Board.board()
+            print(f'Победитель {player}')
+            raise SystemExit
+        Game.free_cells()
