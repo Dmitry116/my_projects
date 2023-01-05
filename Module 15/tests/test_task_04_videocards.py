c85a28c8d6ec19c6c0bde1475a3092d938d13278
Дмитрий Румянцвев <dimon_16rus@bk.ru>
2023-01-05 22:20:49 +0000

    Upload New File

---
+++ Module 15/tests/test_task_04_videocards.py
@@ -0,0 +1,26 @@
+import unittest
+from task_04_videocards.main import select_video_cards
+
+
+class Test04SelectVideoCards(unittest.TestCase):
+    def test_select_video_cards(self):
+        """
+        Проверяем обычный кейс. [3070, 2060, 3090, 3070, 3090]  должны получить [3070, 2060, 3070]
+        """
+        video_cards_list = [3070, 2060, 3090, 3070, 3090]
+        new_video_cards_list = select_video_cards(video_cards_list)
+
+        self.assertEqual(new_video_cards_list, [3070, 2060, 3070])
+
+    def test_select_video_cards_no_result(self):
+        """
+        Проверяем обычный кейс. [3070, 3070]  должны получить []
+        """
+        video_cards_list = [3070, 3070]
+        new_video_cards_list = select_video_cards(video_cards_list)
+
+        self.assertEqual(new_video_cards_list, [])
+
+
+if __name__ == '__main__':
+    unittest.main()
