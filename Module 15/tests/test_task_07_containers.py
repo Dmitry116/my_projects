41058069de24e1c043451e8d2ffe1272bb20ed71
Дмитрий Румянцвев <dimon_16rus@bk.ru>
2023-01-05 22:21:23 +0000

    Upload New File

---
+++ Module 15/tests/test_task_07_containers.py
@@ -0,0 +1,20 @@
+import unittest
+
+from task_07_containers.main import search_serial_number_new_container
+
+
+class Test07SearchSerialNumberNewContainer(unittest.TestCase):
+    def test_main(self):
+        """
+        Проверяем обычный кейс. При вводе [165, 163, 160, 160, 157, 157, 155, 154] должны получить 3
+        """
+        list_container_weights = [165, 163, 160, 160, 157, 157, 155, 154]
+        new_container_weight = 162
+
+        serial_number_new_container = search_serial_number_new_container(list_container_weights, new_container_weight)
+
+        self.assertEqual(serial_number_new_container, 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
