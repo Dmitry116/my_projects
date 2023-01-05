a32e0534b647565168c8e6d38ccdbefb1f761e86
Дмитрий Румянцвев <dimon_16rus@bk.ru>
2023-01-05 22:21:02 +0000

    Upload New File

---
+++ Module 15/tests/test_task_05_movie.py
@@ -0,0 +1,26 @@
+import unittest
+
+from task_05_movie.main import add_favorite_film
+
+
+class Test05AddFavoriteFilm(unittest.TestCase):
+    def test_add_favorite_film(self):
+        """
+        Проверяем обычный кейс. При вводе ["Леон", "Безумный Макс", "Мементо", "Царь горы"]
+         должны получить (["Леон", "Мементо"], ["Безумный Макс", "Царь горы"])
+        """
+        available_films = [
+            "Крепкий орешек", "Назад в будущее", "Таксист",
+            "Леон", "Богемская рапсодия", "Город грехов",
+            "Мементо", "Отступники", "Деревня"
+        ]
+        new_favorite_films = ["Леон", "Безумный Макс", "Мементо", "Царь горы"]
+
+        favorite_films, errors = add_favorite_film(new_favorite_films,available_films )
+
+        self.assertEqual(favorite_films, ["Леон", "Мементо"])
+        self.assertEqual(errors, ["Безумный Макс", "Царь горы"])
+
+
+if __name__ == '__main__':
+    unittest.main()
