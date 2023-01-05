c02af29e562d6604080ae419476c0106b64f2147
Дмитрий Румянцвев <dimon_16rus@bk.ru>
2023-01-05 22:21:55 +0000

    Upload New File

---
+++ Module 15/tests/test_task_10_sort.py
@@ -0,0 +1,16 @@
+import unittest
+
+from task_10_sort.main import sort_list
+
+
+class Test10Sort(unittest.TestCase):
+    def test_sort_list(self):
+        """
+        Проверяем обычный кейс. При вводе [1, 4, -3, 0, 10] должны получить [-3, 0, 1, 4, 10]
+        """
+        sorted_list = sort_list([1, 4, -3, 0, 10])
+        self.assertEqual(sorted_list, [-3, 0, 1, 4, 10])
+
+
+if __name__ == '__main__':
+    unittest.main()
