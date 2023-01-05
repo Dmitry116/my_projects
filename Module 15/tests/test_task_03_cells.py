776f4fe3b3c8665c8f1838f5d98f9f37d8c9b352
Дмитрий Румянцвев <dimon_16rus@bk.ru>
2023-01-05 22:20:33 +0000

    Upload New File

---
+++ Module 15/tests/test_task_03_cells.py
@@ -0,0 +1,25 @@
+import unittest
+
+from task_03_cells.main import select_cells
+
+
+class Test03SelectCells(unittest.TestCase):
+    def test_select_cells(self):
+        """
+        Проверяем обычный кейс. При параметрах [5, 3, 0, 6, 2, 10, 4]  должны получить [0 2, 4]
+        """
+        cells = [3, 0, 6, 2, 10, 4]
+        res_cells = select_cells(cells)
+        self.assertEqual(res_cells, [0, 2, 4])
+
+    def test_select_cells_no_result(self):
+        """
+        Проверяем обычный кейс. При параметрах [1, 2, 3]  должны получить []
+        """
+        cells = [1, 2, 3]
+        res_cells = select_cells(cells)
+        self.assertEqual(res_cells, [])
+
+
+if __name__ == '__main__':
+    unittest.main()
