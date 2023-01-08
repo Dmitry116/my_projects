454f3ae4d350c45e5d4201b5b30e92a8de838195
Professional <dimon_16rus@bk.ru>
2023-01-08 15:34:15 +0300

    home_work_m24. fix 7

--- Module24/07_cohabitation/main.py
+++ Module24/07_cohabitation/main.py
@@ -21,7 +21,7 @@ class Human:
         print('Имя: {}\n'
               'Здоровье: {}\n'
               'Кол-во денег: {}$\n'
-              'Кол-во еды: {}\n'.format(self.name, self.healphy, Home.MONEY, Home.FOOD))
+              'Кол-во еды: {}\n'.format(self.name, self.healphy, self.apartment.MONEY, self.apartment.FOOD))
 
     def pests(self):
         '''Вредители уничтожают еду'''
@@ -35,23 +35,23 @@ class Human:
     def eat(self):
         '''Пополнение здоровья'''
         food = 20
-        if Home.FOOD <= 0:
+        if self.apartment.FOOD <= 0:
             print('Еда закочилась!')
 
-        elif Home.FOOD < food:  # проверка чтобы еда не уходила в минус
-            self.healphy += Home.FOOD
-            Home.FOOD -= Home.FOOD
+        elif self.apartment.FOOD < food:  # проверка чтобы еда не уходила в минус
+            self.healphy += self.apartment.FOOD
+            self.apartment.FOOD -= self.apartment.FOOD
             print(f'Сходил(а) на кухню, поел.\n'
-                  f'Пополнил(а) здоровье на: {Home.FOOD}\n'
+                  f'Пополнил(а) здоровье на: {self.apartment.FOOD}\n'
                   f'Кол-во здровья: {self.healphy}\n'
-                  f'Еды осталось: {Home.FOOD}\n')
+                  f'Еды осталось: {self.apartment.FOOD}\n')
         else:
             self.healphy += food
-            Home.FOOD -= 20
+            self.apartment.FOOD -= 20
             print(f'Сходил(а) на кухню, поел.\n'
                   f'Пополнил(а) здоровье на: {food}\n'
                   f'Кол-во здровья: {self.healphy}\n'
-                  f'Еды осталось: {Home.FOOD}\n')
+                  f'Еды осталось: {self.apartment.FOOD}\n')
 
     def work(self):
         '''Пополнение денег'''
@@ -67,7 +67,7 @@ class Human:
                 paid = random.randint(0, 2)
                 money = Work_message.message[paid][1]
                 healphy = 10
-                Home.MONEY += money
+                self.apartment.MONEY += money
                 self.healphy -= healphy
                 print(f'{Work_message.message[paid][0]}\n'
                       f'Заработал денег: {money}$\n'
@@ -98,39 +98,39 @@ class Human:
         food = 30
         money = 30
         # проверка наличия денег для покупки еды
-        if Home.MONEY < money:
-            money = Home.MONEY
-            food = Home.MONEY
+        if self.apartment.MONEY < money:
+            money = self.apartment.MONEY
+            food = self.apartment.MONEY
         if robbers == 1:
             win = random.randint(0, 1)
             print('По дороге в магазин, напали грабители.\n')
             if win == 0:
                 print('Отобрали все деньги и избили...\n')
                 self.healphy -= 20
-                Home.MONEY = 0
+                self.apartment.MONEY = 0
                 if self.healphy <= 0:
                     print(f'\033[31mВаш питомец {self.name} погиб возле магазина...\033[0m')
                     man.end_game()
                 else:
                     self.eat()
             else:
-                Home.FOOD += food
-                Home.MONEY -= money
+                self.apartment.FOOD += food
+                self.apartment.MONEY -= money
                 print(f'Ваш питомец {self.name} смог отбиться от грабителей!\n'
                       f'Пополнил запасы еды на: {food}\n'
                       f'Потратил денег на еду: {money}$\n'
-                      f'Кол-во еды: {Home.FOOD}\n'
-                      f'Кол-во денег: {Home.MONEY}$\n')
+                      f'Кол-во еды: {self.apartment.FOOD}\n'
+                      f'Кол-во денег: {self.apartment.MONEY}$\n')
                 if self.healphy <= 20:
                     self.eat()
         else:
-            Home.FOOD += food
-            Home.MONEY -= money
+            self.apartment.FOOD += food
+            self.apartment.MONEY -= money
             print(f'Сегодня поход в магазин прошел спокойно.\n'
                   f'Пополнил запасы еды на: {food}\n'
                   f'Потратил денег на еду: {money}$\n'
-                  f'Кол-во еды: {Home.FOOD}\n'
-                  f'Кол-во денег: {Home.MONEY}$\n')
+                  f'Кол-во еды: {self.apartment.FOOD}\n'
+                  f'Кол-во денег: {self.apartment.MONEY}$\n')
             if self.healphy <= 20:
                 self.eat()
 
@@ -140,9 +140,9 @@ class Human:
         raise SystemExit
 
 
-new_appartment = Home()
-man = Human('Артем', 50, new_appartment)
-woman = Human('Мария', 50, new_appartment)
+new_apartment = Home()
+man = Human('Артем', 50, new_apartment)
+woman = Human('Мария', 50, new_apartment)
 
 count_days = 1
 while count_days != 10:
@@ -154,10 +154,10 @@ while count_days != 10:
         if people.healphy <= 20:
             print('Хочу есть! ')
             people.eat()
-        if Home.FOOD <= 20:
+        if new_apartment.FOOD <= 20:
             print('Пошел(а) за продуктами в магазин!')
             people.shooping()
-        if Home.MONEY <= 30:
+        if new_apartment.MONEY <= 30:
             print('Мало денег. Надо найти работу!.')
             people.work()
 
