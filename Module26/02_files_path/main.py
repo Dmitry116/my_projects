c81ea929610b46d952876ef88a317a7da7e454cb
Professional <dimon_16rus@bk.ru>
2023-01-27 15:26:12 +0300

    home_work_m26 done_2_add_doc

--- Module26/02_files_path/main.py
+++ Module26/02_files_path/main.py
@@ -3,6 +3,10 @@ import os
 
 
 def gen_files_path(path):
+
+    """создается файл directory_path.log куда записывается путь всех каталогов
+    os.walk(path) - проходит по всему каталогу указанному пути"""
+
     with open('directory_path.log', 'w', encoding='utf-8') as dir_path:
         for i_path in list(os.walk(path)):
             print(i_path)
