ea845d1388ca79a950d1907259f289cfcc203873
Professional <dimon_16rus@bk.ru>
2023-01-27 15:11:55 +0300

    home_work_m26 done_2

--- Module26/02_files_path/main.py
+++ Module26/02_files_path/main.py
@@ -1,1 +1,13 @@
 # TODO здесь писать код
+import os
+
+
+def gen_files_path(path):
+    with open('directory_path.log', 'w', encoding='utf-8') as dir_path:
+        for i_path in list(os.walk(path)):
+            print(i_path)
+            dir_path.write(str(f'{i_path}\n'))
+
+
+directory_path = r'j:\test'
+gen_files_path(directory_path)
